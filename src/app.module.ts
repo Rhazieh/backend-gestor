6cbefa1a2ce4975d07982eb06e7dcb7a946f2c59
Rhazieh <mcano@alumnos.exa.unicen.edu.ar>
2025-07-19 16:33:50 -0300

    config: conexion a PostgreSQL exitosa, backend funcionando

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
-import { PacientesModule } from './pacientes/pacientes.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { PacientesModule } from './pacientes/pacientes.module';
+import { Paciente } from './pacientes/entities/paciente.entity';
 
 @Module({
-  imports: [PacientesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || 5432),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Paciente],
+      synchronize: true,
+    }),
+    PacientesModule,
+  ],
   controllers: [],
   providers: [],
 })
