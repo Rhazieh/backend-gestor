ed4a1c3fc6d618b72499325a521e6b6057da96cf
Rhazieh <mcano@alumnos.exa.unicen.edu.ar>
2025-07-20 14:31:43 -0300

    feat: DTOs de los pacientes con validaciones

--- src/pacientes/dto/create-paciente.dto.ts
+++ src/pacientes/dto/create-paciente.dto.ts
@@ -1,1 +1,16 @@
-export class CreatePacienteDto {}
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
+// Este DTO se usa cuando queremos crear un nuevo paciente
+export class CreatePacienteDto {
+    @IsNotEmpty() // Valida que no este vacio
+    @IsString() // Valida que sea un texto (String)
+    nombre: string;
+
+    @IsNotEmpty()
+    @IsEmail() // Valida que tenga formato de email (con @ y dominio)
+    email: string;
+
+    @IsNotEmpty()
+    @IsString()
+    telefono: string;
+}
