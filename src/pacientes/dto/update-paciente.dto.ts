855c10519a5b8f92987e860db7b4e3b2d230b265
Maximiliano Germán Cano <mcano@alumnos.exa.unicen.edu.ar>
2025-07-20 18:04:42 -0300

    Update update-paciente.dto.ts

--- src/pacientes/dto/update-paciente.dto.ts
+++ src/pacientes/dto/update-paciente.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePacienteDto } from './create-paciente.dto';
 
+// Este DTO se usa para actualizar un paciente existente.
+// Aprovechamos el DTO de creación, pero usamos PartialType para que todos los campos sean opcionales.
+// Esto significa que se puede actualizar solo un campo (ej: el email) sin necesidad de enviar todos los datos.
+
 export class UpdatePacienteDto extends PartialType(CreatePacienteDto) {}
