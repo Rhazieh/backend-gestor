d9d43d4dcabdcdbed80c4fd18bef11e4a6f22c4e
Maximiliano Germán Cano <mcano@alumnos.exa.unicen.edu.ar>
2025-07-20 18:12:56 -0300

    Update update-turno.dto.ts

--- src/turnos/dto/update-turno.dto.ts
+++ src/turnos/dto/update-turno.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTurnoDto } from './create-turno.dto';
 
+// Este DTO se usa cuando queremos actualizar un turno ya existente.
+// Extendemos de CreateTurnoDto, pero con PartialType:
+// lo que hace PartialType es convertir todos los campos en OPCIONALES.
+// Así no es obligatorio mandar todos los datos, solo los que queramos modificar.
 export class UpdateTurnoDto extends PartialType(CreateTurnoDto) {}
