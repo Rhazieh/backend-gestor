7a896aefe0473fc238bbb42f3d3d914e91d38e0c
Rhazieh <mcano@alumnos.exa.unicen.edu.ar>
2025-07-20 14:49:02 -0300

    feat: DTOs de turnos con validaciones

--- src/turnos/dto/create-turno.dto.ts
+++ src/turnos/dto/create-turno.dto.ts
@@ -1,1 +1,19 @@
-export class CreateTurnoDto {}
+import { IsNotEmpty, IsString, IsDateString, IsInt} from 'class-validator';
+
+export class CreateTurnoDto {
+    @IsNotEmpty()
+    @IsDateString() // Valida que sea una fecha en formato ISO (Ej: 2025-08-15)
+    fecha: string;
+
+    @IsNotEmpty()
+    @IsDateString() // Tambien usamos fecha para la hora, porque TypeORM no tiene "Solo hora"
+    hora: string;
+
+    @IsNotEmpty()
+    @IsString()
+    razon: string;
+
+    @IsNotEmpty()
+    @IsInt() // Debe ser un numero int (el ID del paciente)
+    pacienteId: number; // Este campo se relaciona con el paciente
+}
