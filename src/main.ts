fa4cc9b5161802207249bc16c7b6f086a7b57e6c
Rhazieh <mcano@alumnos.exa.unicen.edu>
2025-08-09 11:02:31 -0400

    api: cors explícito y ValidationPipe global

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
+// backend-gestor/src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common'; // <-- agregar
+import { ValidationPipe } from '@nestjs/common';
 
 async function iniciarApp() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  // Valida DTOs en TODAS las rutas, limpia campos extra y transforma tipos básicos
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   await app.listen(process.env.PORT ?? 3000);
